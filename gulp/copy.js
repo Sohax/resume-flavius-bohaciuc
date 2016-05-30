9a92cb58f998f213ee5691e2e763287825ba9cc4
Marian Zburlea <marianzburlea@gmail.com>
2016-05-30 22:47:32 +0100

    Add update task and update/index.php file for git hook.

--- gulp/copy.js
+++ gulp/copy.js
@@ -7,13 +7,23 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
   var dest = path.join(taskTarget);
 
   // Copy
-  gulp.task('copy', function() {
+  gulp.task('copy', ['update'], function() {
     return gulp.src([
         path.join(dirs.source, '**/*'),
         '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}'),
         '!' + path.join(dirs.source, '**/*.jade')
       ])
       .pipe(plugins.changed(dest))
+      .pipe(gulp.dest(dest));
+  });
+
+  // Copy
+  gulp.task('update', function() {
+    console.log('***!');
+    console.log(dirs.source + '/_update');
+    return gulp.src([
+        path.join(dirs.source, '_update/index.html')
+      ])
       .pipe(gulp.dest(dest));
   });
 };
---
+++ src/update/index.php
@@ -0,0 +1,1 @@
+<?php `cd /var/www/resume-flavius-bohaciuc && /usr/bin/git pull 2>&1 && gulp --production`;
